aa22c348bc6a6dcd8313fddf27dfbd5f0a8b95c5
Italo Silva Guanilo <italosilva@uni.pe>
2022-07-26 00:42:10 -0500

    Pregunta 2

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import sys, os
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__== '__main__':
-    app.run(debug=True, port = 5000)
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=3000)
